0a1700a4e0a7e83c9061dccddf9b7b04d7d810a2
Suman Rai <mack30324@gmail.com>
2024-07-18 11:17:59 +0545

    change

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose');
-const Product = require('./models/product.model.js');
-const productRoute = require('./routes/product.route.js');
+const Product = require('./models/Product.js');
+const productRoute = require('./routes/productRoute.js');
 const app = express()
 const port = 4000;
 
@@ -12,6 +12,7 @@ app.use(express.urlencoded({extended: false}));
 
 //routes
 app.use("/api/products", productRoute);
+app.use("/api/users", userRoute);
 
 
 
